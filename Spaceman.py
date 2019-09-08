3926074c59be6712abed4f66827675e04f8a2e1a
Snixerz <cakeisew@gmail.com>
2019-09-08 14:05:10 -0700

    Fixed some bugs, added some more

--- Spaceman.py
+++ Spaceman.py
@@ -10,27 +10,31 @@ def load_word():
     return secret_word
 
 def split(secret_word):
-  return list(secret_word)
-  print(split(word))
+  pass
 
 def get_guessed_word(secret_word, letters_guessed):
-    blank_space = "_" * len(secret_word)
-    print(blank_space)
+  blank_space = ""
+  combine = ""
+  for letter in secret_word:
+    blank_space += "_"
+  print(blank_space)
+  for x in temp_2:
+    combine+= x
+  print(combine)
+  return "HELLO"
 
 def is_guess_in_word(guess, secret_word):
-
-  if len(guess) != 1:
-    print("You can only guess one letter per turn, try again!")
-  elif guess in secret_word:
-    print("That letter is in the secret word!")
-  else:
-    print("That letter is not in the secret word!")
-    value[1] -= 1
-  if value[1] == 0:
-    print("You have lost the game, the word was: " + str(secret_word))
-  guess1 = set(split(secret_word))
-  if guess in guess1:
-    str.replace(guess, blank_space)
+  index = 0
+  result = True
+  for x in secret_word:
+    if(guess == x):
+      temp_2[index] = x
+    else:
+      result = False
+    index += 1
+  if(result == False):
+    value[1] -=1
+  return temp_2
 
 def spaceman(secret_word):
     introduction = f"The word contains {value[0]} letters. You have a total of 7 turns per game. You only have {value[1]} before you lose. Try again!"
@@ -38,11 +42,13 @@ def spaceman(secret_word):
 
 secret_word = load_word()
 value = [len(secret_word), 7]
+temp_2 = []
+for x in range(value[0]):
+  temp_2.append("_")
 while value[1] is not 0:
     spaceman(secret_word)
     guess = input("Guess a letter: ")
-    is_guess_in_word(guess, secret_word)
+    print(is_guess_in_word(guess, secret_word))
     get_guessed_word(secret_word, 0)
 else:
   print("You have lost the game! The word was:" +str(secret_word))
-
